256f11cd6b26750ff558d4e2c3f10fa716886491
Lexi <alexamapes13@gmail.com>
2022-08-25 16:35:20 -0400

    Add operate() function

--- javascript.js
+++ javascript.js
@@ -17,3 +17,20 @@ function divide(x, y) {
     let result = x / y;
     return result;
 }
+
+function operate(a, operator, b) {
+    let operateResult;
+    if (operator == "+") {
+        operateResult = add(a, b);
+    }
+    else if (operator == "-") {
+        operateResult = subtract(a, b);
+    }
+    else if (operator == "*") {
+        operateResult = multiply(a, b);
+    }
+    else {
+        operateResult = divide(a, b);
+    }
+    return operateResult;
+}
