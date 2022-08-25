4f0f60d49548c88505639fd6c44776d3873046b9
Lexi <alexamapes13@gmail.com>
2022-08-25 17:43:57 -0400

    Add ability to add multiple numbers

--- javascript.js
+++ javascript.js
@@ -1,19 +1,32 @@
+let lastNum = 0;
+let currentNum = 0;
+let currentOperation = "";
+let isFirstClick = true;
+
 function add(x, y) {
+    x = Number(x);
+    y = Number(y);
     let result = x + y;
     return result;
 }
 
 function subtract(x, y) {
+    x = Number(x);
+    y = Number(y);
     let result = x - y;
     return result;
 }
 
 function multiply(x, y) {
+    x = Number(x);
+    y = Number(y);
     let result = x * y;
     return result;
 }
 
 function divide(x, y) {
+    x = Number(x);
+    y = Number(y);
     let result = x / y;
     return result;
 }
@@ -41,25 +54,27 @@ output.classList.add("outputNum");
 output.textContent = "";
 outputContainer.appendChild(output);
 
-let lastNum = 0;
-let currentNum = 0;
-let currentOperation = "";
-
 const numButtons = document.querySelectorAll(".num");
 numButtons.forEach(function(button) {
     button.addEventListener("click", function() {
         output.textContent += button.value;
         currentNum = output.textContent;
+        isFirstClick = false;
     });
 });
 
 const addButton = document.querySelector(".add");
 addButton.addEventListener("click", function() {
-    lastNum = currentNum;
-    currentNum = 0;
-    currentOperation = "+";
-    output.textContent = "";
-    console.log(lastNum, currentNum, currentOperation);
+    if (isFirstClick == true) {
+        return 0;
+    }
+    else {
+        lastNum = currentNum;
+        currentNum = 0;
+        currentOperation = "+";
+        output.textContent = "";
+        console.log(lastNum, currentNum, currentOperation);
+    };
 });
 
 const subButton = document.querySelector(".subtract");
@@ -79,7 +94,10 @@ divButton.addEventListener("click", function() {
 
 const equalButton = document.querySelector(".equals");
 equalButton.addEventListener("click", function() {
-    console.log("equalize");
+    result = operate(lastNum, currentOperation, currentNum);
+    currentNum = result;
+    output.textContent = result;
+    console.log(lastNum, currentOperation, currentNum);
 })
 
 const clearButton = document.querySelector(".clear");
